bd13e3a42f95f551414161b34a065967536fcb92
Yaroslav <krive1ka.in0000@gmail.com>
2022-10-25 23:05:10 +0300

    Task03 Change

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -8,22 +8,25 @@ int day =  Convert.ToInt32(Console.ReadLine());
 
 if(day == 1){
     Console.WriteLine("Понедельник");
-};
-if(day == 2){
+}
+else if(day == 2){
     Console.WriteLine("Вторник");
-};
-if(day == 3){
+}
+else if(day == 3){
     Console.WriteLine("Среда");
-};
-if(day == 4){
+}
+else if(day == 4){
     Console.WriteLine("Четверг");
-};
-if(day == 5){
+}
+else if(day == 5){
     Console.WriteLine("Пятница");
-};
-if(day == 6){
+}
+else if(day == 6){
     Console.WriteLine("Суббота");
-};
-if(day == 7){
+}
+else if(day == 7){
     Console.WriteLine("Воскресенье");
-};
+}
+else{
+    Console.WriteLine("В недели от 1 до 7 !");
+}
