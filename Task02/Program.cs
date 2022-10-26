ac3a7a8b5a81eec52a410b00c78aba2a588414ec
Yaroslav <krive1ka.in0000@gmail.com>
2022-10-26 23:58:26 +0300

    Task02 HomeWork Done

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -5,3 +5,18 @@
 // a = 5; b = 7 -> max = 7
 // a = 2; b = 10 -> max = 10
 // a = -9; b = -3 -> max = -3
+
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.Write($"Максимальное число: {a}");
+}
+else
+{
+    Console.Write($"Максимальное число: {b}");
+}
