ed12ae3e77407cb60c6bbe5e193b4647bc5cac60
Yaroslav <krive1ka.in0000@gmail.com>
2022-11-01 13:28:27 +0300

    Task09 Выполнел при помощи Метода

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -9,14 +9,24 @@
 int number = new Random().Next(10, 100);
 Console.WriteLine($"Случайное число из отрезка 10 - 99 => {number}");
 
-int a = number / 10;
-int b = number % 10;
+// int a = number / 10;
+// int b = number % 10;
 
-if(a > b)
-{    
-    Console.WriteLine($"Из числа {number} , наибольшее цифра числа => {a}");
-}
-else
+// if (a > b)
+// {
+//     Console.WriteLine($"Из числа {number} , наибольшее цифра числа => {a}");
+// }
+// else
+// {
+//     Console.WriteLine($"Из числа {number} , наибольшее цифра числа => {b}");
+// }
+
+int MaxDigital(int number)
 {
-    Console.WriteLine($"Из числа {number} , наибольшее цифра числа => {b}");
+    int a = number / 10;
+    int b = number % 10;
+    return a > b ? a : b;
 }
+int rez = MaxDigital(number);
+
+Console.WriteLine($"Из числа {number} , наибольшее цифра числа => {rez}");
