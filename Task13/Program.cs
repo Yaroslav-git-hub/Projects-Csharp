d0f471201a3a41d17ef81b09c3e80cd8ac1435a0
Yaroslav <krive1ka.in0000@gmail.com>
2022-11-03 23:38:35 +0300

    Task13 HomeWork Done

---
+++ Task13/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 13: Напишите программу, которая выводит
+// третью цифру заданного числа или сообщает, что третьей
+// цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите Трёхзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+while (number > 999)
+{
+    number = number / 10;
+}
+
+if (number > 99)
+{
+    int res = number % 10;
+    Console.Write(res);
+}
+else
+{
+    Console.Write("Третьей цифры нет");
+}
+
+
+
+
+
+
