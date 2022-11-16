dd5528f79bd613ac97944c79418176a974d3f8de
Yaroslav <krive1ka.in0000@gmail.com>
2022-11-16 14:28:29 +0300

    Task24 Done

---
+++ Task24/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 24: Напишите программу, которая
+// принимает на вход число (А) и выдаёт сумму чисел
+// от 1 до А.
+
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int res = Method(number);
+Console.Write($"Сумма цифр числа {number} -> {res}");
+
+int Method(int a)
+{
+    int res = 0;
+    if (a > 0)
+    {
+        for (int i = 0; i <= a; i++)
+        {
+            res = res + i;
+        }
+    }
+    return res;
+}
